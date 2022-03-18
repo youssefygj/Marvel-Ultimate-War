da5e2e35abfd1b6e2154b0e274fa0c94d9b974ac
Youssef <yousseftyoh@gmail.com>
2022-03-18 22:07:39 +0200

    youssef

---
+++ src/src/engine/Player.java
@@ -0,0 +1,34 @@
+package engine;
+
+import model.world.Champion;
+
+import java.util.ArrayList;
+
+public class Player {
+    private String name;
+    private Champion leader;
+    private ArrayList<Champion> team;
+
+    public Player(String name) {
+
+        this.name = name;
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public Champion getLeader() {
+
+        return leader;
+    }
+
+    public void setLeader(Champion leader) {
+        this.leader = leader;
+    }
+
+    public ArrayList<Champion> getTeam() {
+        return team;
+    }
+}
--- src/src/model/test.java
+++
@@ -1,7 +0,0 @@
-package model;
-
-public class test {
-    public static void main(String[] args) {
-        System.out.println("heh");
-    }
-}
