602e4fa5731006db49e96f3e48d8f21e2f9103ea
Youssef <yousseftyoh@gmail.com>
2022-05-04 15:07:32 +0200

    Changes by macbookpro

--- src/src/model/abilities/HealingAbility.java
+++ src/src/model/abilities/HealingAbility.java
@@ -1,5 +1,6 @@
 package model.abilities;
 
+import model.world.Champion;
 import model.world.Damageable;
 
 import java.io.IOException;
@@ -28,9 +29,9 @@ public class HealingAbility extends Ability {
     public void execute(ArrayList<Damageable> targets){
 
         for(int i=0;i<targets.size();i++){
-
+            if(targets.get(i) instanceof Champion){
             targets.get(i).setCurrentHP(targets.get(i).getCurrentHP()+this.getHealAmount());
 
         }}
 
-}
+}}
